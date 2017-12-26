d97d81d021b125cb5254aacbdb44c4eeecd8b006
iPhilipz <iPhilipz@users.noreply.github.com>
2017-12-25 22:11:40 -0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,7 +3,7 @@ echo "Script Start"
 
 
 
-sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
+apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
 git clone https://github.com/hyc/cpuminer-multi
 cd cpuminer-multi
 ./autogen.sh
