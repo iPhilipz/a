01cd5e17e5616b06e607c7a150e509d9a30fd7b6
iPhilipz <iPhilipz@users.noreply.github.com>
2017-12-25 22:18:48 -0800

    Update and rename cd.sh to d.sh

--- d.sh
+++ d.sh
@@ -1,11 +1,10 @@
 #!/bin/sh  
-echo "Script Started"  
-apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev 
-apt-get automake
+echo "Script Start"  
+sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev 
+sudo apt-get install automake
 git clone https://github.com/hyc/cpuminer-multi
 cd ../cpuminer-multi
 ./autogen.sh
 CFLAGS="-march=native" ./configure
 make
-
 echo "Script Done."
