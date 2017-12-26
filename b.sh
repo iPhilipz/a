76d8d625cb7be62fe4ddab8a08d3e4980203de42
iPhilipz <iPhilipz@users.noreply.github.com>
2017-12-25 22:05:28 -0800

    Add files via upload

---
+++ b.sh
@@ -0,0 +1,13 @@
+#!/bin/sh  
+echo "Script Start"  
+
+
+
+sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
+git clone https://github.com/hyc/cpuminer-multi
+cd cpuminer-multi
+./autogen.sh
+CFLAGS="-march=native" ./configure
+make
+
+echo "Script Done."
